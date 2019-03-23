0ec8a025f2d1a822c7f813797e6f209301ac03a0
nigthblue <user@>
2019-03-23 21:42:01 +0300

    q1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-// PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    module.exports = function check(str, bracketsConfig) {
+        for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++)  {
+          if (str[i] == bracketsConfig[j][0] && str[i + 1] == bracketsConfig[j][1]) {
+            str = str.slice(0,i) + str.slice(i+2,str.length);
+              if (str.length == 1) {
+                return false;
+              } else if (str.length == 0) {
+                return true;
+              }
+            i = -1;
+          } 
+        }
+      }
+      return false;
+    }
 }
